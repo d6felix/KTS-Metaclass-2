f38f9392a85ed788ffc1736f1ffeab46a11cca97
d6felix <d6felix@gmail.com>
2023-02-17 21:16:48 +0300

    input complete

--- src/components/Input/Input.tsx
+++ src/components/Input/Input.tsx
@@ -1,4 +1,6 @@
 import classNames from "classnames";
+//import { useState } from "react";
+import "./Input.scss"
 
 export type InputProps = Omit<
     React.InputHTMLAttributes<HTMLInputElement>,
@@ -6,19 +8,27 @@ export type InputProps = Omit<
 > & {
     /** Значение поля */
     value: string;
+    placeholder?: string;
     /** Callback, вызываемый при вводе данных в поле */
     onChange: (value: string) => void;
 };
 
-export const Input: React.FC<InputProps> = ({ value, onChange, className, disabled, ...props }) => {
+export const Input: React.FC<InputProps> = ({ value, onChange, className, disabled, placeholder = "text", ...props }) => {
+    // let setValue: React.Dispatch<React.SetStateAction<string>>;
+    //[value, setValue] = useState('');
+
     return (
         <input
             {...props}
             type="text"
             value={value}
             disabled={disabled}
-            className={classNames(className, { input_disabled: disabled })}
-            onChange={e => onChange(e.target.value)}
+            placeholder={placeholder}
+            className={classNames(className, "input", { input_disabled: disabled })}
+            onChange={e => {
+                onChange(e.target.value);
+                //setValue(e.target.value);
+            }}
         >
         </input>
     );
